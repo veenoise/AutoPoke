5462c15ace34ee12ec651c12e0f17fca0d3a057b
veenoise <william.eduard123@gmail.com>
2024-03-04 13:27:45 +0800

    First commit

---
+++ scripts/content.js
@@ -0,0 +1,13 @@
+setInterval(e => {
+	location.reload();
+
+	var x = document.getElementsByClassName("x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft");
+
+	for (let i = 0; i < x.length; i++) {
+		if (x[i].innerHTML === "Poke Back") {
+			x[i].click();
+		}
+	}
+}, 3000);
+
+
